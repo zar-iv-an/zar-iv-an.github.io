8500d0f67929c1e3ac4a6531f05a3c49ef697d94
zar-iv-an <82267248+zar-iv-an@users.noreply.github.com>
2021-04-16 14:17:37 +0200

    Add files via upload

---
+++ Torte/app.js
@@ -0,0 +1,39 @@
+ VANTA.WAVES({
+   el: "#info",
+   mouseControls: true,
+   touchControls: true,
+   gyroControls: false,
+   minHeight: 200.00,
+   minWidth: 200.00,
+   scale: 1.00,
+   scaleMobile: 1.00,
+   color: 0x938145
+ }) 
+/*  VANTA.WAVES({
+   el: "#info",
+   mouseControls: true,
+   touchControls: true,
+   gyroControls: false,
+   minHeight: 200.00,
+   minWidth: 200.00,
+   scale: 1.00,
+   scaleMobile: 1.00,
+   color: 0x1b1b1b,
+   shininess: 59.00,
+   waveHeight: 16.00,
+   zoom: 0.90
+ }) */
+/*  VANTA.WAVES({
+   el: "#info",
+   mouseControls: true,
+   touchControls: true,
+   gyroControls: false,
+   minHeight: 200.00,
+   minWidth: 200.00,
+   scale: 1.00,
+   scaleMobile: 1.00,
+   color: 0x464e70,
+   shininess: 59.00,
+   waveHeight: 16.00,
+   zoom: 0.90
+ }) */
